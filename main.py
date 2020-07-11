ca1d36e9fe8be7b447ed56ccaf68302b529f3d35
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-07-12 09:56:29 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord,datetime,time,os,pymongo,dns,keep_alive,asyncio,flask,threading
 from discord.ext import commands, tasks
 from itertools import cycle
 
+
 mongosecret=os.environ.get("mongosecret")
 client = pymongo.MongoClient(mongosecret)
 db=client["bot"]
@@ -34,6 +35,32 @@ status=cycle([";help", "cat and mouse with raiders"])
 async def change_status():
   await bot.change_presence(activity=discord.Game(next(status)))
 
+@tasks.loop(seconds=3600)
+async def updatedb():
+  try:
+    collection=db["prefix"]
+    collection.insert_one({"_id":guild.id,"prefix":";"})
+  except pymongo.errors.DuplicateKeyError:
+    pass
+  try:
+    collection=db["logs"]
+    collection.insert_one({"_id":guild.id,"mode":"off"})
+  except pymongo.errors.DuplicateKeyError:
+    pass
+  try:
+    collection=db["warns"]
+    collection.insert_one({"_id":guild.id})
+    venimute = await guild.create_role(name="mootmoot")
+    for channel in guild.text_channels:
+      await channel.set_permissions(venimute,send_messages=False)
+  except pymongo.errors.DuplicateKeyError:
+    pass
+  try:
+    collection=db["raidprotection"]
+    collection.insert_one({"_id":guild.id,"seconds":0,"message":"None"})
+  except pymongo.errors.DuplicateKeyError:
+    pass
+
 @bot.event
 async def on_ready():
     change_status.start()
@@ -44,34 +71,25 @@ async def on_ready():
   
 @bot.event
 async def on_guild_join(guild):
-  collection=db["prefix"]
-  collection.insert_one({"_id":guild.id,"prefix":";"})
-  collection=db["invites"]
+  collection=db["logs"]
   collection.insert_one({"_id":guild.id,"mode":"off"})
   collection=db["warns"]
   collection.insert_one({"_id":guild.id})
+  venimute = await guild.create_role(name="mootmoot")
+  for channel in guild.text_channels:
+    await channel.set_permissions(venimute,send_messages=False)
+  collection=db["raidprotection"]
+  collection.insert_one({"_id":guild.id,"seconds":0,"message":"None"})
 
 @bot.event
 async def on_guild_remove(guild):
   collection=db["prefix"]
   collection.delete_one({"_id":guild.id})
-  collection=db["invites"]
+  collection=db["logs"]
   collection.delete_one({"_id":guild.id})
 
-@bot.event
-async def on_member_join(member):
-  if member.guild.id!=431906396032991232:
-    return
-  current_time=time.time()
-  member_created=member.created_at.timestamp()
-  if current_time - member_created < 1814400:
-    role = discord.utils.get(member.guild.roles, id=587216429233733632)
-    await asyncio.sleep(1.5)
-    await member.add_roles(role)
-    channel=discord.utils.get(member.guild.channels,id=552609849939329027)
-    await channel.send(f"Account {member.name} was created less than 3 weeks ago and was muted successfully")
-    await member.send("Due to your account being created recently, you have been automuted. \
-                \n To gain access to the server, DM <@575252669443211264> for access and the staff team will respond as soon as possible. ")
+  
+
 
 @bot.event
 async def on_message(message):
@@ -109,6 +127,7 @@ async def invite(ctx):
 
 @bot.command()
 async def uptime(ctx):
+    global start_time
     current_time = time.time()
     difference = int(round(current_time - start_time))
     text = str(datetime.timedelta(seconds=difference))
@@ -131,7 +150,7 @@ async def reload(ctx,*,file=None):
 
 
 
-
+'''
 @bot.command(aliases=["prefix","newprefix"])
 @commands.has_permissions(administrator=True)
 async def setprefix(ctx,newprefix):
@@ -146,7 +165,7 @@ async def setprefix_error(ctx,error):
   if isinstance(error,commands.MissingPermissions):
     await ctx.send("You do not have permission to change my prefix! You need admin to do so.")
   elif isinstance(error,commands.MissingRequiredArgument):
-    await ctx.send("You did not specify what to change the prefix to!")
+    await ctx.send("You did not specify what to change the prefix to!")'''
 
   
 @bot.command()
