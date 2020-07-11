36bf9ce1040df8b337f02c01353773a1383699da
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-07-11 16:53:48 +1200

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -39,7 +39,8 @@ class Events(commands.Cog):
       if channel!=0:
         channel=discord.utils.get(member.guild.channels,id=channel)
         timenow=datetime.utcnow()
-        embed=discord.Embed(title=member,description=f"{member} has joined the server",color=0xffff00,timestamp=timenow)
+        embed=discord.Embed(title=f"{member} has joined the server",description=f"{member} has joined the server",color=0xffff00,timestamp=timenow)
+        embed.set_thumbnail(url=member.avatar_url)
         await channel.send(embed=embed)
 
   @commands.Cog.listener()
@@ -51,7 +52,8 @@ class Events(commands.Cog):
       if channel!=0:
         channel=discord.utils.get(member.guild.channels,id=channel)
         timenow=datetime.utcnow()
-        embed=discord.Embed(title=member,description=f"{member} has left the server",color=0xffff00,timestamp=timenow)
+        embed=discord.Embed(f"{member} has left the server",description=f"{member} has left the server",color=0xffff00,timestamp=timenow)
+        embed.set_thumbnail(url=member.avatar_url)
         await channel.send(embed=embed)    
   '''
   @commands.Cog.listener()
@@ -76,20 +78,49 @@ class Events(commands.Cog):
  
     n = after.display_name 
     if n!=before.display_name:
-      embed=discord.Embed(title="Member nickname changed",description=f"{before.display_name} --> {after.display_name}",color=0xffff00,timestamp=timenow)
+      embed=discord.Embed(title=f"{after} nickname changed",description=f"{before.display_name} --> {after.display_name}",color=0xffff00,timestamp=timenow)
+      embed.set_thumbnail(url=after.avatar_url)
       await channel.send(embed=embed)
 
-		elif before.roles != after.roles:
-			embed = discord.Embed(title="Role updates for a member",
-						  color=0xffff00,
-						  timestamp=datetime.utcnow())
-			fields = [("Before", ", ".join([r.mention for r in before.roles]), False),
-					  ("After", ", ".join([r.mention for r in after.roles]), False)]
+    if before.roles!=after.roles:
+      embed = discord.Embed(title=f"Role updates for {after}",
+      color=0xffff00,
+      timestamp=datetime.utcnow())
+      embed.set_thumbnail(url=after.avatar_url)
+      after_roles=[r.mention for r in after.roles if r not in before.roles]
+      before_roles=[r.mention for r in before.roles if r not in after.roles]
 
-			for name, value, inline in fields:
-				embed.add_field(name=name, value=value, inline=inline)
-      await ctx.send(embed=embed)
+      if len(after_roles)>0:
+        embed.add_field(name=f"+{len(after_roles)} roles",value=", ".join(after_roles),inline=False)
+      if len(before_roles)>0:
+        embed.add_field(name=f"-{len(before_roles)} roles",value=", ".join(before_roles),inline=False)
+      await channel.send(embed=embed)
+    
 
+  @commands.Cog.listener()
+  async def on_message_edit(self,before,after):
+    collection=db["logs"]
+    a=collection.find_one({"_id": before.guild.id})
+    if a["mode"]!="on":
+      return
+      
+    channel=a["channel"]
+    if channel==0:
+      return
+    channel=discord.utils.get(after.guild.channels,id=channel)
+    
+    if before.content != after.content:
+      embed = discord.Embed(title=f"Message edited in {after.channel}", description=f"Edit by {after.author}.",#.display_name
+      colour=0xffff00,
+      timestamp=datetime.utcnow())
+      embed.set_thumbnail(url=after.avatar_url)
+      fields = [("Before", before.content, False),
+      ("After", after.content, False)]
+
+      for name, value, inline in fields:
+        embed.add_field(name=name, value=value, inline=inline)
+
+      await channel.send(embed=embed)
 
   
   @commands.Cog.listener()
@@ -102,8 +133,9 @@ class Events(commands.Cog):
         channel=discord.utils.get(message.guild.channels,id=channel)
         timenow=datetime.utcnow() 
         embed=discord.Embed(title=f"A message by {message.author} was deleted in {message.channel}",description=message.content,color=0xffff00,timestamp=timenow)
+        embed.set_thumbnail(url=message.author.avatar_url)
         await channel.send(embed=embed)
-  
+
   @commands.Cog.listener()
   async def on_bulk_message_delete(self,messages):
     collection=db["logs"]
@@ -116,6 +148,177 @@ class Events(commands.Cog):
         embed=discord.Embed(title=f"Messages bulk deleted in {messages.channel}",description=len(messages),color=0xffff00,timestamp=timenow)
 
         await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_user_update(self, before, after):
+    collection=db["logs"]
+    for guild in self.bot.guilds:
+      a=collection.find_one({"_id":guild.id})
+      if a["mode"]!="on":
+        return
+      user=discord.utils.get(guild.members, id=after.id)
+      if not user:
+        pass
+      else:
+        channel=a["channel"]
+        if channel==0:
+          return
+        channel=discord.utils.get(guild.channels,id=channel)
+
+        if before.name != after.name:
+          embed = discord.Embed(title=f"Username change by {after}",
+          colour=0xffff00,
+          timestamp=datetime.utcnow())
+          embed.set_thumbnail(url=after.avatar_url)
+          fields = [("Before", before, False),#.name
+          ("After", after, False)]
+
+          for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+          embed.set_thumbnail(url=after.avatar_url)
+          await channel.send(embed=embed)
+
+        if before.discriminator != after.discriminator:
+          embed = discord.Embed(title=f"{after} changed their discriminator",
+          colour=0xffff00,
+          timestamp=datetime.utcnow())
+          embed.set_thumbnail(url=after.avatar_url)
+
+          fields = [("Before", before.discriminator, False),
+                ("After", after.discriminator, False)]
+
+          for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+
+          await channel.send(embed=embed)
+
+        if before.avatar_url != after.avatar_url:
+          embed = discord.Embed(title="Avatar change",
+          colour=self.log_channel.guild.get_member(after.id).colour,
+          timestamp=datetime.utcnow())
+
+          embed.set_thumbnail(url=before.avatar_url)
+          embed.set_image(url=after.avatar_url)
+          await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_guild_channel_create(self,channel):
+    collection=db["logs"]
+    a=collection.find_one({"_id":channel.guild.id})
+    if a["mode"]!="on":
+      return
+      
+    channel=a["channel"]
+    if channel==0:
+      return
+    channel=discord.utils.get(channel.guild.channels,id=channel)
+    embed=discord.Embed(title="A channel was created in {channel.category}",description=f"{channel.mention} was created at {channel.created_at}",timestamp=datetime.utcnow())
+    embed.add_field(name="Channel overwrites:",value=channel.overwrites,inline=False)
+    await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_guild_channel_delete(self,channel):
+    collection=db["logs"]
+    a=collection.find_one({"_id":channel.guild.id})
+    if a["mode"]!="on":
+      return
+      
+    channel=a["channel"]
+    if channel==0:
+      return
+    channel=discord.utils.get(channel.guild.channels,id=channel)
+    embed=discord.Embed(title=f"A channel was deleted in {channel.category}", 
+    description=f"{channel.mention} was deleted at {channel.created_at}",timestamp=datetime.utcnow())
+    await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_guild_channel_update(self,before,after):
+    pass
+
+
+  @commands.Cog.listener()
+  async def on_webhooks_update(self,channel):
+    pass
+
+  @commands.Cog.listener()
+  async def on_guild_role_create(self,role):
+    collection=db["logs"]
+    a=collection.find_one({"_id":role.guild.id})
+    if a["mode"]!="on":
+      return
+      
+    channel=a["channel"]
+    if channel==0:
+      return
+
+    channel=discord.utils.get(role.guild.channels,id=channel)
+
+
+    embed=discord.Embed(title="A role was created",description=f"{role.mention} was created at {role.created_at}\nPermissions: {role.permissions}",timestamp=datetime.utcnow())
+    await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_guild_role_delete(self,role):
+    collection=db["logs"]
+    a=collection.find_one({"_id":role.guild.id})
+    if a["mode"]!="on":
+      return
+      
+    channel=a["channel"]
+    if channel==0:
+      return
+
+    channel=discord.utils.get(role.guild.channels,id=channel)
+
+
+    embed=discord.Embed(title="A role was deleted",description=f"{role.mention} was deleted\nPermissions: {role.permissions}",timestamp=datetime.utcnow())
+    await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_guild_role_update(self,before,after):
+    pass
+
+  @commands.Cog.listener()
+  async def on_member_ban(self,guild,user):
+    collection=db["logs"]
+    a=collection.find_one({"_id":guild.id})
+    if a["mode"]!="on":
+      return
+      
+    channel=a["channel"]
+    if channel==0:
+      return
+
+    channel=discord.utils.get(guild.channels,id=channel)
+
+    embed=discord.Embed(title=f"{user} was banned!",description=f"{user} must have been a bad boy/girl!",timestamp=datetime.utcnow())
+
+    await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_member_unban(self,guild,user):
+    collection=db["logs"]
+    a=collection.find_one({"_id":guild.id})
+    if a["mode"]!="on":
+      return
+      
+    channel=a["channel"]
+    if channel==0:
+      return
+
+    channel=discord.utils.get(guild.channels,id=channel)
+
+    embed=discord.Embed(title=f"{user} was unbanned!",description="I wonder why they were banned in the first place",timestamp=datetime.utcnow())
+
+    await channel.send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_invite_create(self,invite):
+    pass
+  #https://discordpy.readthedocs.io/en/latest/api.html#discord.on_raw_message_edit
+
+
+
 
 def setup(bot):
   bot.add_cog(Events(bot))
